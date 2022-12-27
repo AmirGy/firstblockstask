37e450d5d1b38bf8e6e27ceb4826d5d77ff04414
Amir <firstharrypotter1@gmail.com>
2022-12-28 02:00:55 +0600

    solved all tasks

--- task/Program.cs
+++ task/Program.cs
@@ -15,6 +15,22 @@ string[] FillPrintArray(string[] firstArray, int length)
     return firstArray;
 }
 
+void SecondArray(string[] firstArray)
+{
+    int size = new Random().Next(1, 4);
+    string[] newArray = new string[size];
+    Console.WriteLine("Второй массив: ");
+    for (int i = 0; i < size; i++)
+    {
+        int number = new Random().Next(0, size);
+        newArray[i] = firstArray[number];
+        Console.Write($"{newArray[i]} ");
+    }
+}
+
 Console.Write("Введите размер массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 string[] firstArray = new string[length];
+FillPrintArray(firstArray, length);
+SecondArray(firstArray);
+
