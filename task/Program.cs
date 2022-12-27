73bd9dfff508aa5f9106314184d4b47a6a27fa94
Amir <firstharrypotter1@gmail.com>
2022-12-28 01:58:29 +0600

    second step

--- task/Program.cs
+++ task/Program.cs
@@ -2,3 +2,14 @@
 int length = Convert.ToInt32(Console.ReadLine());
 string[] firstArray = new string[length];
 
+for (int j = 0; j < length; j++)
+    {
+        Console.Write("Введите слово: ");
+        string word = Convert.ToString(Console.ReadLine());
+        firstArray[j] = word;
+    }
+    Console.WriteLine();
+    Console.WriteLine("Первоначальный массив: ");
+    for (int k = 0; k < length; k++)
+        Console.Write($"{firstArray[k]} ");
+    Console.WriteLine();
