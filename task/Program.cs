d3f25686fc313861e1a48d5bce5c1055fd45afd0
Amir <firstharrypotter1@gmail.com>
2023-01-06 00:56:53 +0600

    correction

--- task/Program.cs
+++ task/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-string[] FillPrintArray(string[] firstArray, int length)
+string[] FillPrintArray(string[] firstArray)
 {
-    for (int j = 0; j < length; j++)
+    for (int j = 0; j < firstArray.Length; j++)
     {
         Console.Write("Введите слово: ");
         string word = Convert.ToString(Console.ReadLine());
@@ -9,28 +9,48 @@ string[] FillPrintArray(string[] firstArray, int length)
     }
     Console.WriteLine();
     Console.WriteLine("Первоначальный массив: ");
-    for (int k = 0; k < length; k++)
+    for (int k = 0; k < firstArray.Length; k++)
         Console.Write($"{firstArray[k]} ");
     Console.WriteLine();
     return firstArray;
 }
 
-void SecondArray(string[] firstArray)
+int Checking(string[] firstArray)
 {
-    int size = new Random().Next(1, 4);
-    string[] newArray = new string[size];
-    Console.WriteLine("Второй массив: ");
-    for (int i = 0; i < size; i++)
+    int j = 0;
+    for (int i = 0; i < firstArray.Length; i++)
     {
-        int number = new Random().Next(0, size);
-        newArray[i] = firstArray[number];
-        Console.Write($"{newArray[i]} ");
+        int l = 0;
+        foreach (var item in firstArray[i])
+            l++;
+        if (l <= 3)
+            j++;
+    }
+    return j;
+}
+
+void FillSecondArray(string[] firstArray, string[] newArray)
+{
+    int k = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        int l = 0;
+        foreach (var item in firstArray[i])
+            l++;
+        if (l <= 3)
+        {
+            newArray[k] = firstArray[i];
+            Console.Write($"{newArray[k]} ");
+            k++;
+        }
     }
 }
 
 Console.Write("Введите размер массива: ");
 int length = Convert.ToInt32(Console.ReadLine());
 string[] firstArray = new string[length];
-FillPrintArray(firstArray, length);
-SecondArray(firstArray);
+FillPrintArray(firstArray);
+string[] newArray = new string[Checking(firstArray)];
+FillSecondArray(firstArray, newArray);
+
 
